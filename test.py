0d6da7ffb43fabcf782582edce5c92c84655e0de
natdanaisriapai <natdainai.7901@gmail.com>
2026-02-12 20:48:58 +0700

    Add test script for data loading and basic function execution in Python

---
+++ test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('/Users/toptap_n/Desktop/basic-data-science/data/raw/house_california_new.csv')
+
+print(df)
+
+def main():
+    a = 10
+    print(a)
+
+if __name__ == "__main__":
+    main()
